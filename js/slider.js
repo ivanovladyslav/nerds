0b527cf1983d546b0e352906c0cbe323dadb88e7
Ivanov Vladislav <ivanovladyslav@gmail.com>
2018-03-01 22:46:25 +0700

    Added loop for slider

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,8 @@ $(document).ready(function($) {
 	var imageBox = $('.slider-block ul');
 	var imageWidth = $('.slider-block ul li').first().width();
 	var imageQuantity = $('.slider-block ul').children('li').length;
+	var clickedButton = 1;
+	var timerSlide;
 
 	var navButtonFirst = $('<div data-nav="data-nav" data-count="'+1+'" class="slider-btn slider-choosed"></div>');
 	$('.nav').append(navButtonFirst);
@@ -12,18 +14,34 @@ $(document).ready(function($) {
 		$('.nav').append(navButton);
 	}
 
-	$('.nav div').on('click', function() {
-		var clickedButton = $(this).data('count');
-
+	function moveSlider(slideToMove) {
+		clickedButton = $(slideToMove).data('count');
 		$(prevSlide).removeClass('slider-choosed');
 		$(prevSlide).addClass('slider-notchoosed');
-		$(this).removeClass('slider-notchoosed');
-		$(this).addClass('slider-choosed');
-		prevSlide = this;
-
+		$(slideToMove).removeClass('slider-notchoosed');
+		$(slideToMove).addClass('slider-choosed');
+		prevSlide = slideToMove;
 		var animateValue = -(clickedButton-1)*(imageWidth+100);
 		imageBox.animate({
 			'left' : animateValue
 		})
+	}
+
+	function slideLoop() {
+		if(clickedButton<3) {
+			clickedButton++;
+		} else {
+			clickedButton = 1;
+		}
+		moveSlider($('.slider-btn[data-count="'+clickedButton+'"]'));
+	}
+	timerSlide = setInterval(slideLoop, 2000);
+
+	$('.nav div').on('click', function() {
+		moveSlider(this);
+		clearTimeout(timerSlide);
+		timerSlide = setInterval(slideLoop, 2000);
 	});
+
+
 });
